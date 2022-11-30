177fa79d28d99fd05142cecb3c767661ef95a1bc
Иванов Алексей <89289075559@mai.ru>
2022-11-30 10:37:46 +0300

    task 4

---
+++ HW_Lesson2/task_4.py
@@ -0,0 +1,11 @@
+n = int(input("В ведите число: "))
+list_n = []
+for i in range(-abs(n), abs(n) + 1):
+    list_n.append(i)
+print(list_n)
+prod = 1
+path = 'file.txt'
+with open(path, 'r') as data:
+    for line in data:
+        prod *= list_n[int(line)]
+    print(prod)
--- HW_Lesson2/task_5.py
+++ HW_Lesson2/task_5.py
@@ -1,1 +1,6 @@
-#
+# curtime = str(datetime.datetime.now())[-6:]
+import datetime
+
+randomNum = datetime.datetime.now().microsecond%10
+
+print (f'Случайное число: {(randomNum) }')
